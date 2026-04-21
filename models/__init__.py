a8fccc850f7bc3a6ed29fd2a37324f2fb927baba
Dechang Xu <dechang64@users.noreply.github.com>
2026-04-21 18:06:51 +0800

    Delete models directory

--- models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .vit import ViTBackbone, ViTConfig, build_vit, VIT_CONFIGS
-from .mae import MaskedAutoencoder, build_mae
-from .prototype import PrototypeBank, PrototypeAwareAggregator
-from .heads import ClassificationHead, SegmentationHead, DetectionHead, build_head
-from .unet import UNetDecoder, AttentionGate

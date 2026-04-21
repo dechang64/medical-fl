02fccf37c6497fc34da313f29b4840392b537256
Dechang Xu <dechang64@users.noreply.github.com>
2026-04-21 18:07:03 +0800

    Delete fl directory

--- fl/__init__.py
+++
@@ -1,3 +0,0 @@
-from .server import FLServer, ClientUpdate, AggregationResult, RoundHistory
-from .client import FLClient, MAEClient, ClientConfig
-from .aggregation import Aggregator, FedAvg, FedProx, FedBN, ScaffoldAggregator, build_aggregator
